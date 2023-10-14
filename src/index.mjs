9369b77edc82d8d749bdc223532dcfa86c446bd3
Harukamori <147842081+haruka-mori03@users.noreply.github.com>
2023-10-14 08:52:37 +0000

    Update index.mjs 23/1014/17:52

--- src/index.mjs
+++ src/index.mjs
@@ -58,3 +58,16 @@
 
 //アロー関数
 //従来の関数
+// function func1(str) {
+//     return str;
+// }
+const func1 = function(str) {
+    return str;
+}
+console.log(func1("func1です"));
+
+//アロー関数
+const func2 = (str) => {
+    return str;
+}
+console.log(func2("func2です"));

1fe706ee2c5c246024b41cc466d3994a55e59d84
Harukamori <147842081+haruka-mori03@users.noreply.github.com>
2023-10-15 00:54:26 +0000

    Update index.mjs 23/1015/09:54

--- src/index.mjs
+++ src/index.mjs
@@ -61,13 +61,16 @@
 // function func1(str) {
 //     return str;
 // }
-const func1 = function(str) {
-    return str;
-}
-console.log(func1("func1です"));
+// const func1 = function(str) {
+//     return str;
+// }
+// console.log(func1("func1です"));
 
-//アロー関数
-const func2 = (str) => {
-    return str;
-}
-console.log(func2("func2です"));
+// //アロー関数
+// const func2 = str => {
+//     return str;
+// }
+// console.log(func2("func2です"));
+
+const func3 = (num1, num2) => num1 + num2;
+console.log(func3(10,20));

708248cc05a5b6de49759fe760ab9843a01314d1
Harukamori <147842081+haruka-mori03@users.noreply.github.com>
2023-10-15 02:33:30 +0000

    Update index.mjs　23/1015/11:33

--- src/index.mjs
+++ src/index.mjs
@@ -88,10 +88,37 @@
 // const message2 =`名前は${name}です。年齢${age}は歳です。`;
 // console.log(message2);
 
-const myProfile =["森",21];
-const message3 = `名前は${myProfile[0]}で、年齢は${myProfile[1]}歳です。`;
-console.log(message3);
+// const myProfile =["森",21];
+// const message3 = `名前は${myProfile[0]}で、年齢は${myProfile[1]}歳です。`;
+// console.log(message3);
 
-const [name, age] = myProfile;
-const message4 = `名前は${name}です。年齢は${age}歳です。`;
-console.log(message4);
+// const [name, age] = myProfile;
+// const message4 = `名前は${name}です。年齢は${age}歳です。`;
+// console.log(message4);
+
+//デフォルト値、引数など
+// const sayHello = (name = "ゲスト") => console.log(`こんにちは！${name}さん！`);
+// sayHello("はるか");
+
+//スプレッド構文...
+//配列の展開
+// const arr1 = [1,2];
+// // console.log(arr1);
+// // console.log(...arr1);
+
+// const sumFunc = (num1, num2) => console.log(num1 + num2);
+//     sumFunc(arr1[0],arr1[1]);
+//     sumFunc(...arr1);
+
+//まとめる
+// const arr2 = [1, 2, 3, 4, 5];
+// const [num1, num2, ...arr3] = arr2;
+// console.log(num1);
+// console.log(num2);
+// console.log(arr3);
+
+//配列のコピー、結合
+const arr4 = [10, 20];
+const arr5 = [30, 40];
+
+const arr6 = [...arr4];

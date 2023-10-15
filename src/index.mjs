3ab8c106341a2211dc076f724c93890e01c08d05
Harukamori <147842081+haruka-mori03@users.noreply.github.com>
2023-10-15 01:19:43 +0000

    Update index.mjs 23/1015/10:19

--- src/index.mjs
+++ src/index.mjs
@@ -72,5 +72,26 @@
 // }
 // console.log(func2("func2です"));
 
-const func3 = (num1, num2) => num1 + num2;
-console.log(func3(10,20));
+// const func3 = (num1, num2) => num1 + num2;
+// console.log(func3(10,20));
+
+//分割代入
+// const myProfile = {
+//     name:"森はるか",
+//     age:21,
+// }
+
+// const message1 =`名前は${myProfile.name}です。年齢${myProfile.age}は歳です。`;
+// console.log(message1);
+
+// const {name, age} = myProfile;
+// const message2 =`名前は${name}です。年齢${age}は歳です。`;
+// console.log(message2);
+
+const myProfile =["森",21];
+const message3 = `名前は${myProfile[0]}で、年齢は${myProfile[1]}歳です。`;
+console.log(message3);
+
+const [name, age] = myProfile;
+const message4 = `名前は${name}です。年齢は${age}歳です。`;
+console.log(message4);
